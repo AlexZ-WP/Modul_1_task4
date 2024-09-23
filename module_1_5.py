a772254003af3c205229ebfbe20d118f129c5fd9
AlexZ-WP <alexz10@mail.ru>
2024-09-23 20:15:46 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+immutable_var = 1, 10, True, "Serial", ([21, "apple"])
+print('Immutable_tuple: ', immutable_var)
+mutable_list = [1, 2, True, 'Serial', 21, 'apple']
+mutable_list.append('Modified')
+print('Mutable list: ', mutable_list)
